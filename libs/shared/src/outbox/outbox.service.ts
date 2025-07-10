ec3555835ab699599b7cb28e3081179fc6214fa5
Masud Mirzazada <masudmirzazada@gmail.com>
2025-07-10 19:02:31 +0400

    add session to create outbox service

--- libs/shared/src/outbox/outbox.service.ts
+++ libs/shared/src/outbox/outbox.service.ts
@@ -1,6 +1,11 @@
+import { ClientSession } from "mongoose";
 import IOutbox from "./outbox.interface";
 import { Outbox } from "./outbox.model";
 
-export const createOutbox = async (body: Partial<IOutbox>): Promise<IOutbox> => {
-  return await Outbox.create(body);
+export const createOutbox = async (
+  body: Partial<IOutbox>,
+  session: ClientSession,
+): Promise<IOutbox> => {
+  const result = new Outbox(body);
+  return await result.save({ session });
 };
